4e624e285a66610976d2ce18392506a261a265f3
fedir72 <fedir72@ua.fm>
2020-08-05 20:37:21 +0300

    added else 3 examples

--- 57_complicatedInterfases_UIcollView/57_complicatedInterfases_UIcollView/CompositionalVC.swift
+++ 57_complicatedInterfases_UIcollView/57_complicatedInterfases_UIcollView/CompositionalVC.swift
@@ -22,7 +22,7 @@ class CompositionalVC: UIViewController {
     
     func setupVieContr() {
         
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout03())
         collectionView.autoresizingMask = [.flexibleWidth,.flexibleHeight]
         collectionView.backgroundColor = #colorLiteral(red: 0.9334655855, green: 0.9334655855, blue: 0.9334655855, alpha: 1)
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
@@ -31,6 +31,61 @@ class CompositionalVC: UIViewController {
         collectionView.dataSource = self
     }
     
+ //MARK: - 01 example layout
+    private func createLayout() -> UICollectionViewLayout {
+        
+        //section -> groups -> items -> size
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.2), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize )
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.2))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
+    }
+    
+    //MARK: - 02 example layout
+       private func createLayout02() -> UICollectionViewLayout {
+           
+           //section -> groups -> items -> size
+           
+           let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.2), heightDimension: .fractionalHeight(1.0))
+           let item = NSCollectionLayoutItem(layoutSize: itemSize )
+        //отступы
+        item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+        
+           let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.2))
+           let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+           
+           let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
+           let layout = UICollectionViewCompositionalLayout(section: section)
+           
+           return layout
+       }
+    //MARK: - 03 example layout
+          private func createLayout03() -> UICollectionViewLayout {
+              //section -> groups -> items -> size
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+             let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.5))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item,count: 2)
+            //расстояние между группами
+            let spasing = CGFloat(10)
+            group.interItemSpacing = .fixed(spasing)
+            let section = NSCollectionLayoutSection(group: group)
+            section.interGroupSpacing = spasing
+            section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: spasing, bottom: 0, trailing: spasing)
+              let layout = UICollectionViewCompositionalLayout(section: section)
+              
+              return layout
+          }
+    
+    
     
     
 }
@@ -38,28 +93,25 @@ class CompositionalVC: UIViewController {
 
 extension CompositionalVC: UICollectionViewDelegate,UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return 11
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
         cell.backgroundColor = .green
+        cell.layer.borderWidth = 2
+        cell.layer.cornerRadius = 15
         return cell
     }
     
     
 }
 
-//MARK: - UICollectionViewDelegateFlowLayout
 
-extension CompositionalVC: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 100
-        )
-    }
-}
 
-//MARK: - подключение канваса через свифтЮАй ( вместо симулятора)
+
+
+//MARK: - подключение канваса через свифтЮАй (вместо симулятора)
 
 import SwiftUI
 
